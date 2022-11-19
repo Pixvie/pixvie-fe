87d991ae506210841c42f0e1b96ab7881a0b01f2
Szqii <szqii.altn@gmail.com>
2022-11-19 18:05:44 +0300

    Update :: Implementing Panzoom

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,45 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div class="container"></div>
 </template>
 
+<script setup>
+import panzoom from "panzoom";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const el = document.querySelector(".container");
+  panzoom(el, {
+    maxZoom: 2,
+    minZoom: 0.2,
+    smoothScroll: true,
+    bounds: true,
+    boundsPadding: 0.1,
+    zoomDoubleClickSpeed: 1,
+  });
+});
+</script>
+
 <style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100vw;
+  height: 100vh;
+
+  overflow: hidden;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.container {
+  background-color: #2c3e50;
+  width: 2000px;
+  height: 2000px;
 }
 </style>
