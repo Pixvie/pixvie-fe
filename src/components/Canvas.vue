9ab86d33c80b4ce3ee6a46da9d2c233a00f4d1a8
Altay Simsek <altaysimsek16@gmail.com>
2022-12-04 06:50:09 +0300

    ree

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,5 +1,9 @@
 <template>
-  <canvas id="canvas" width="2000" height="2000"></canvas>
+  <canvas
+    id="canvas"
+    width="2000"
+    height="2000"
+  ></canvas>
   <div
     class="container"
     id="container"
@@ -22,7 +26,7 @@ import io from "socket.io-client";
 const socket = ref(null);
 // import PixelsContainer from "@/components/PixelsContainer.vue";
 onMounted(() => {
-  socket.value = io("https://pixvie.tech:3000"); //
+  socket.value = io("http://35.242.209.225:3000/"); //
   socket.value.on("DRAWED_PIXEL", ({ x, y, color }) => {
     drawPixel(x, y, color, false);
   });
@@ -51,8 +55,8 @@ onMounted(async () => {
     zoomSpeed: 0.1,
   });
 
-  const data = await fetch("https://pixvie.tech:3000/api/board").then((res) =>
-    res.json()
+  const data = await fetch("http://35.242.209.225:3000//api/board").then(
+    (res) => res.json()
   );
 
   for (let i = 0; i < 2000; i += 10) {
@@ -89,7 +93,7 @@ async function drawPixel(x, y, color, flag = true) {
   if (flag) {
     socket.value.emit("DRAW_PIXEL", { x, y, color });
     console.log(x, y);
-    await fetch("https://pixvie.tech:3000/api/board", {
+    await fetch("http://35.242.209.225:3000//api/board", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
