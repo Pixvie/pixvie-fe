d1647add5c07e6ac87216a02f6887a0935469143
Altay Simsek <altay@jotform.com>
2023-01-25 15:06:46 +0300

    fix: path

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -24,7 +24,7 @@ const socket = ref(null);
 // import PixelsContainer from "@/components/PixelsContainer.vue";
 onMounted(() => {
   socket.value = io("https://pixvie.tech", {
-    path: "/app/socket.io/",
+    path: "/api/socket.io/",
     rejectUnauthorized: false,
   }); //
   socket.value.on("DRAWED_PIXEL", ({ x, y, color }) => {
