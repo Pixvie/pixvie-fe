13d1e0902b76bf342c67065e97efebc165723c72
Altay Simsek <altay@jotform.com>
2023-05-05 17:54:48 +0300

    fix(quill): typo on import

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 export const socket = io(`${process.env.VUE_APP_API_SERVER}`, {
-  path: "/socket/",
+  path: "/api/socket/",
   rejectUnauthorized: false,
 }); //
