4d3aeb28b8c4f97b2a3fccd4378f0b577399538a
Altay Simsek <altay@jotform.com>
2023-05-05 17:58:11 +0300

    fix: socket con

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-export const socket = io(`${process.env.VUE_APP_API_SERVER}`, {
+export const socket = io("https://pixvie.tech", {
   path: "/api/socket/",
   rejectUnauthorized: false,
 }); //
