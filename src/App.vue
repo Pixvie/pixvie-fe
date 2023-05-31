dcd581956ca40afc6348b2106ccead44a6901da9
Szqii <szqii.altn@gmail.com>
2023-05-31 10:07:00 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  background-color: #cd45fa;
+  background-color: #ff006a;
   width: 100vw;
   height: 100vh;
   overflow: hidden;
