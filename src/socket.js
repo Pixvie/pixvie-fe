01eadc490c45d05030a229363f886ce3c18497d0
Altay Simsek <altaysimsek16@gmail.com>
2023-06-04 03:05:43 +0300

    feat: update socket conf

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 export const socket = io(process.env.VUE_APP_SOCKET_SERVER, {
-  path: "/api/socket/",
+  path: "/socket/",
   rejectUnauthorized: false,
   autoConnect: false,
 }); //
