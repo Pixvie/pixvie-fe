c10069b2f2bb6fb7acb6796bf84038fd561e76da
Altay Simsek <altaysimsek16@gmail.com>
2023-06-04 03:13:59 +0300

    fix: socket connection k8s

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
+console.log(process.env.VUE_APP_SOCKET_SERVER);
+console.log(process.env.VUE_APP_API_SERVER);
+
 // "undefined" means the URL will be computed from the `window.location` object
 export const socket = io(process.env.VUE_APP_SOCKET_SERVER, {
   path: "/socket/",
